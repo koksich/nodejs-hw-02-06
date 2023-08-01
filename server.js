487c9a39bb2641265654f25e8a410b0a8b6c3131
Natasha Kuksa <natasha.kuksa.96@gmail.com>
2023-08-01 18:02:59 +0300

    upd server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Nata:FMIWu85y9FZoVFVq@cluster0.iffdq5t.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("DataBase connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+    // proces.exit(1) - команда, що закриває запущені процеси 
+  });
